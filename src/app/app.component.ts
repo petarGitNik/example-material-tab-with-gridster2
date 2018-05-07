f61e087b7e65c9419dfecd0e7159b801622e3676
Petar Nikolovski <petarGitNik@users.noreply.github.com>
2018-05-07 16:14:45 +0200

    Add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  /*
+   * Each time tab is switched, call Gridster api to resize itself. This is
+   * necessary in order for grids to render correctly (or to render at all).
+   */
   public onChange(event, ...tabComponents): void {
     for (let tab of tabComponents) {
       tab.options.api.resize();
